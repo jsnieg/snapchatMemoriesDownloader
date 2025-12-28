1f989212b9d424f3f2e16644b8b18919718c7b9a
jsnieg <jsnieg52635@gmail.com>
2025-12-28 14:29:33 +0000

    added last modified of the file, there is 7 days expiry date on memories to be downloaded as right now when fetching urls it throws 403 forbidden

--- src/base.py
+++ src/base.py
@@ -7,9 +7,7 @@ import requests
 
 import re
 
-import threading
-
-from http.server import HTTPServer, BaseHTTPRequestHandler, SimpleHTTPRequestHandler
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 from functools import partial
 
 from bs4 import BeautifulSoup, Tag
@@ -18,17 +16,21 @@ from typing import LiteralString
 
 from dataclasses import dataclass
 
-from threading import Thread
+from urllib.request import urlretrieve
 
+from datetime import datetime, timedelta
+
+from statx import statx
 from . import main
 
 @dataclass
 class Config:
     port: int = 8000
-    directory: str = '../mydata/'
+    directory: str = '../mydata'
     memories: LiteralString = f'http://localhost:{port}/html/memories_history.html'
 
 config = Config()
+urls: list[str] = []
 
 def create_http_server() -> HTTPServer:
     """Create HTTP server at a given port by the config dataclass (struct)."""
@@ -39,11 +41,23 @@ def create_http_server() -> HTTPServer:
     except:
         raise "Server could not be created."
 
-def general_information() -> None:
+def general_information():
     """Function printing general information about the script. 7 days left to download your data, etc."""
-    print('')
+    # https://stackoverflow.com/questions/237079/how-do-i-get-file-creation-and-modification-date-times
+    dt = None
     if platform.system() == 'Windows':
-        last_modified: float = os.path.getmtime(...)
+        dt = os.path.getmtime(config.directory)
+    else:
+        stat = os.stat(config.directory)
+        try:
+            dt = datetime.fromtimestamp(stat.st_birthtime)
+        except AttributeError as exception:
+            btime = statx(config.directory).btime
+            if btime: 
+                dt = datetime.fromtimestamp(btime)
+
+    expiry = dt + timedelta(days=7)
+    return
 
 def get_raw_links(tag: Tag) -> str:
     """Get raw download links when a Tag from BS4 is passed through. This will let _download_memories(parameter)_ download all wanted files."""
@@ -72,28 +86,29 @@ def run_beautiful_soup() -> None:
     print(f'Found {len(a_tags)} images and videos!')
     for tags in a_tags:
         tag = tags.get('onclick')
-        print(get_raw_links(tag))
-    print("Done")
+        urls.append(get_raw_links(tag))
+    print(urls[0])
+    # urlretrieve(urls[0], 'testfile')
+    print(f'last modified: {general_information()}')
 
-class LinkExtractor():
-    ...
-
-# class ThreadCreator():
-#     def server_thread(self, func) -> Thread:
-#         return threading.Thread(target="")
-
-#     def soup_thread(self, func) -> Thread:
-#         return threading.Thread(target="")
-
-class SnapchatMemoriesDownloader():
-    def __init__(self):
-        # self.threads = ThreadCreator()
-        ...
+# class SnapchatMemoriesDownloader(Config):
+#     def __init__(self):
+#         self.tags: list[str] = []
         
-    def run(self) -> None:
-        # self.threads.server_thread()
-        # self.threads.soup_thread()
-        ...
 
-def download_memories() -> None:
-    ...
+#     def scrape(self) -> None:
+#         """The brains behind this script. Runs BS4, looping through raw HTML finding all tags required to find download links. Creates new Thread instance working separately."""
+#         time.sleep(1.5)
+#         page: str = get_webpage_text(config.memories)
+#         soup = BeautifulSoup(markup=page, features='html.parser')
+#         table = soup.find("tbody")
+#         a_tags = table.find_all_next('a')
+#         print(f'Found {len(a_tags)} images and videos!')
+#         for tags in a_tags:
+#             tag = tags.get('onclick')
+#             self.tags.append(tag)
+#         print("Done")
+
+
+#     def download_memories() -> None:
+#         ...
--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import threading
 from . import base
 
 if __name__ == "__main__":
-    # url_thread = threading.Thread(target=base.getWebPageText)
+    config = base.Config()
     server_thread = threading.Thread(target=base.run_server)
     beautifulSoup_thread = threading.Thread(target=base.run_beautiful_soup)
 
